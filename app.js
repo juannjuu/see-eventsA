ae0840f9e0e587df5733f23b30bfbb5e7d60cc0b
Juannjuu <juanrahmadhika@gmail.com>
2022-01-25 23:31:02 +0700

    + deploy prep

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        status: 'OK',
+        message: 'see-event-teama is running',
+        time: new Date.now().toLocaleString()
+    })
+})
+
 app.use('/api/v1', routes);
 
 app.listen(port, () => {
