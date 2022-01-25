e5f92e39e72ec820c2cab92f9fa2b7865c06dfd3
Juannjuu <juanrahmadhika@gmail.com>
2022-01-25 22:42:01 +0700

    + deploy prep

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
 
 app.use('/api/v1', routes);
 
