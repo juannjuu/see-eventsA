5bd41e2b85f9c4c08983d5a1698959a361250c1e
Juannjuu <juanrahmadhika@gmail.com>
2022-01-26 10:24:54 +0700

    + add cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const routes = require('./routes');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors())
 app.get("/", (req, res) => {
     res.status(200).json({
         status: 'OK',
