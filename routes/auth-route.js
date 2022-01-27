1ffea82291e590c3664cfa4776f3bae1d91b6163
affandiagung <ccoc.20001@gmail.com>
2022-01-28 05:05:43 +0700

    ! facebook

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -2,14 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {
     register,
-    login,
-    facebookCallback
+    login,facebookCallback
 } = require("../controllers/authController");
 const passport = require("passport")
 require('../utils/passport');
-const {
-    generateToken
-} = require("../utils/jwt")
+const { generateToken } = require("../utils/jwt")
 
 router.post("/register", register);
 router.post("/login", login);
@@ -17,41 +14,27 @@ router.post("/login", login);
 router.get('/google', passport.authenticate('google', {
     scope: ['email', 'profile'],
     session: false,
-}));
-
-router.get('/google/callback', passport.authenticate('google', {
-        failureRedirect: 'https://www.rakyat.xyz/login',
-        session: false,
-    }),
-    function (req, res) {
-        let payload = {
-            id: req.user.id,
-            email: req.user.email,
-        };
-
-        const access_token = generateToken(payload);
-        res.status(200).json({
-            access_token
-        });
-    }
+    })
 );
 
-router.get("/facebook", passport.authenticate("facebook", {
-    scope: ['email']
-}))
-
-router.get("/facebook/callback", passport.authenticate('facebook', {
-    failureRedirect: 'https://www.rakyat.xyz/login'
-}), function (req, res) {
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: 'https://www.rakyat.xyz/login',
+    session: false,
+}),
+function (req, res) {
     let payload = {
-        id: req.user.id,
-        email: req.user.email,
+    id: req.user.id,
+    email: req.user.email,
     };
 
     const access_token = generateToken(payload);
-    res.status(200).json({
-        access_token
-    });
-})
+    res.status(200).json({ access_token });
+}
+);
+
+router.get("/facebook",passport.authenticate("facebook",{scope: ['email'] } ))
+router.get("/facebook/callback",passport.authenticate('facebook', { failureRedirect: 'https://www.rakyat.xyz/login' }), facebookCallback)
+
+
 
 module.exports = router;
