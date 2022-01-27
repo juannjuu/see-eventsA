1656855e97a2b64bafb23d4ba48c155390fe2e68
Juannjuu <juanrahmadhika@gmail.com>
2022-01-27 09:06:03 +0700

    + add google and fb oauth

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
     register,
-    login,facebookCallback
+    login,
+    facebookCallback
 } = require("../controllers/authController");
 const passport = require("passport")
 require('../utils/passport');
-const { generateToken } = require("../utils/jwt")
+const {
+    generateToken
+} = require("../utils/jwt")
 
 router.post("/register", register);
 router.post("/login", login);
@@ -14,28 +17,35 @@ router.post("/login", login);
 router.get('/google', passport.authenticate('google', {
     scope: ['email', 'profile'],
     session: false,
-    })
-);
+}));
 
 router.get('/google/callback', passport.authenticate('google', {
-    failureRedirect: 'https://www.rakyat.xyz/login',
-    session: false,
-}),
-function (req, res) {
-    let payload = {
-    id: req.user.id,
-    email: req.user.email,
-    };
+        failureRedirect: 'https://www.rakyat.xyz/login',
+        session: false,
+    }),
+    function (req, res) {
+        let payload = {
+            id: req.user.id,
+            email: req.user.email,
+        };
 
-    const access_token = generateToken(payload);
-    res.status(200).json({ access_token });
-}
+        const access_token = generateToken(payload);
+        res.status(200).json({
+            access_token
+        });
+    }
 );
 
-router.get("/facebook",async (req,res)=>{
-    res.status(200).json({ msg : "error"})
-},passport.authenticate("facebook",{scope: ['email'] } ))
-router.get("/facebook/callback",passport.authenticate('facebook', { failureRedirect: 'https://www.rakyat.xyz/login' }), facebookCallback)
+router.get("/facebook", async (req, res) => {
+    res.status(200).json({
+        msg: "error"
+    })
+}, passport.authenticate("facebook", {
+    scope: ['email']
+}))
+router.get("/facebook/callback", passport.authenticate('facebook', {
+    failureRedirect: 'https://www.rakyat.xyz/login'
+}), facebookCallback)
 
 
 
